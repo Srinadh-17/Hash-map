13726572ddd0abe2ea3157567a1d127c526acb34
Srinadh-17 <145363859+Srinadh-17@users.noreply.github.com>
2023-10-29 10:45:53 +0530

    Add files via upload

---
+++ Longest_consecutive_sequence.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        st = set(nums)    
+        mx = 0
+        for num in nums:
+            if num-1 not in st:
+                tmp = 1
+                while num+1 in st:
+                    tmp += 1
+                    num += 1
+                mx = max(mx, tmp)
+   
+        return mx
+        
